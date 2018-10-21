164a659eb52bdb6df3e64ee1217412cb0ddc80dc
Jorge <joitorresca@unal.edu.co>
2018-10-21 11:42:11 -0500

    Inicio del diccionario de funciones, se completaron las funciones inc y dec
     Cambios a ser confirmados:
    	modificado:     Z80CPU.py

--- Z80CPU.py
+++ Z80CPU.py
@@ -63,9 +63,54 @@ def ld(opA, opB):
         registros[opA] = opB
 
 def inc(opA):
+    varInc = registros[opA]
+    if len(opA) == 8:
+        aux = int(varInc,2) + int('1',2)
+        varInc = Rell_Zeros(bin(aux))
+        registros[apA] = varInc
+    elif len(opA) == 16:
+        p1 = varInc[0:8]
+        P2 = varInc[8:16]
+        if p2 == '11111111':
+            p2 = '00000000'
+            aux = int(p1,2) + int('1',2)
+            p1 = Rell_Zeros(bin(aux))
+        else:
+            aux = int(p2,2) + int('1',2)
+            p2 = Rell_Zeros(bin(aux))
+        varInc = ''
+        varInc = p1 + p2
+        registros[apA] = varInc
+    #F[6]='0'
+    #F[5]='1'
+
+
     # TODO: Incrementar Funcion
 def dec(opA):
+    varDec = registros[opA]
+    if len(opA) == 8:
+        aux = int(varDec,2) - int('1',2)
+        varDec = Rell_Zeros(bin(aux))
+        registros[apA] = varDec
+    elif len(opA) == 16:
+        p1 = varDec[0:8]
+        P2 = varDec[8:16]
+        if p2 == '00000000':
+            p2 = '11111111'
+            aux = int(p1,2) - int('1',2)
+            p1 = Rell_Zeros(bin(aux))
+        else:
+            aux = int(p2,2) - int('1',2)
+            p2 = Rell_Zeros(bin(aux))
+        varDec = ''
+        varDec = p1 + p2
+        registros[apA] = varDec
+    #F[6]='0'
+    #F[5]='1'
+
     # TODO: Decrementar Funcion
+
+
 
 def update():
     registros['BC'] = registros['B'] + registros['C']
@@ -106,7 +151,7 @@ def execute(instr):
                 ld(letters[y], byte2)
             if q == '1':
                 # TODO: ADD este implica manejar los flags, nada grave.
-
+                a=0 #solo para dejar compilar
         # Indirect loading
         if z == '010':
             if q == '0':
@@ -137,18 +182,105 @@ def execute(instr):
 
         # 8-bit INC
         if z == '100':
-
+            a=0 #solo para dejar compilar
         # 8-bit DEC
         if z == '101':
-
+            a=0 #solo para dejar compilar
         # 8-bit load immediate
         if z == '110':
             ld(y, z)
-            
+
         # Assorted operations on accumulator/flags
         if z == '111':
-
+            a=0 #solo para dejar compilar
     update()
 
 execute(instr3)
 print(registros)
+
+
+#El diccionario de funciones va despues de declarar las funciones
+#Ejemplo de un diccionario de funciones
+"""
+
+def suma(a,b):
+    return a+b
+def resta(arg):
+    return arg-1
+
+var1=int(input('ingrese un numero'))
+var2=3
+arg=6
+dic={'1':suma(var1,var2),'2':resta(arg)}
+print('diccionario de funciones', dic['1'])
+
+"""
+
+dicFunciones = {
+    'ADC':'ADC',
+    'ADD':'ADD',
+    'AND':'AND',
+    'BIT':'BIT',
+    'CALL':'CALL',
+    'CCF':'CCF',
+    'CP':'CP',
+    'CPD':'CPD',
+    'CPDR':'CDPR',
+    'CPI':'CPI',
+    'CPIR':'CIR',
+    'CPL':'CPL',
+    'DAA':'DAA',
+    'DEC':"DEC",
+    'DI':'DI',
+    'DJNZ':'DJNZ',
+    'EI':'EI',
+    'EX':'EX',
+    'EXX':'EXX',
+    'HALT':'HALT',
+    'IM':'IM',
+    'IN':'IN',
+    'INC':'INC',
+    'IND':'IND',
+    'INDR':'INDR',
+    'INI':'INI',
+    'INIR':'INIR',
+    'JP':'JP',
+    'JR':'JR',
+    'LD':ld(ld1, ld2),
+    'LDD':'LDD',
+    'LDDR':'LDDR',
+    'LDI':'LDI',
+    'LDIR':'LDIR',
+    'NEG':'NEG',
+    'NOP':'NOP',
+    'OR':'OR',
+    'OTDR':'OTDR',
+    'OUT':'OUT',
+    'OUTD':'OUTD',
+    'OUTI':'OUTI',
+    'POP':'POP',
+    'PUSH':'PUSH',
+    'RES':'RES',
+    'RET':'RET',
+    'RETI':'RETI',
+    'RETN':'RETN',
+    'RL':'RL',
+    'RLA':'RLA',
+    'RLC':'RLC',
+    'RLCA':'RLCA',
+    'RLD':'RLD',
+    'RR':'RR',
+    'RRA':'RRA',
+    'RRC':'RRC',
+    'RRCA':'RRCA',
+    'RRD':'RRD',
+    'RST':'RST',
+    'SBC':'SBC',
+    'SCF':'SCF',
+    'SET':'SET',
+    'SLA':'SLA',
+    'SRA':'SRA',
+    'SLR':'SLR',
+    'SUB':'SUB',
+    'XOR':'XOR'
+}
